9443b10c311d8d5cee985281e5fcd337828575f5
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2020-12-07 23:26:56 +0500

    Add controller and model
    Add the resource project controller and customeize it with the settings for model.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-App\Http\Controllers\ProjectController;
+use App\Http\Controllers\ProjectController;
 
 /*
 |--------------------------------------------------------------------------

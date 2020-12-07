1221194f936ec2ad8664537f8604405e5d84a443
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2020-12-07 23:11:53 +0500

    Add resource route
    Add the resource route for the new project functionality.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+App\Http\Controllers\ProjectController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Projects routes
+Route::resource('projects', ProjectController::class);
+
